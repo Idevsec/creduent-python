85a5f7d85605097d08023116bbbcda7fb6bdd141
Kashish Kanojia <cyberfascinate@gmail.com>
2026-06-28 21:42:32 +0530

    chore: bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="creduent",
-    version="2.0.1",
+    version="2.0.2",
     author="IDevSec",
     url="https://github.com/idevsec/creduent-python",
     packages=find_packages(),
